1ac4f86112541a7980d1f8362d1dec20c06a53e8
iranjith <rgranjith89@gmail.com>
2024-05-06 06:59:58 +0530

    useState hook example

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,13 +1,15 @@
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
-export async function getProducts(category) {
+const getProducts = async (category)=> {
   const response = await fetch(baseUrl + "products?category=" + category);
   if (response.ok) return response.json();
   throw response;
 }
 
-export async function getProduct(id) {
+const getProduct = async (id) => {
   const response = await fetch(baseUrl + "products/" + id);
   if (response.ok) return response.json();
   throw response;
 }
+
+export { getProduct,getProducts };
