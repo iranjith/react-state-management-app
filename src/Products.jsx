ab06796209838243827b40cf7ad11f1acd859919
iranjith <rgranjith89@gmail.com>
2024-05-07 05:44:41 +0530

    Declaring and Reading URL placeholders

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
+import { useParams } from "react-router-dom";
 
 export default function Products() {
   const [size, setSize] = useState("");
+  const { category } = useParams();
   const {
     data: products,
     loading,
     error,
-  } = useFetch("products?category=shoes");
+  } = useFetch("products?category=" + category);
 
   const onSizeChange = (e) => {
     setSize(e.target.value);
