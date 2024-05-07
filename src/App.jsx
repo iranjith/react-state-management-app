509a4001b5aedd1eeb9168530178b4fe832122a9
iranjith <rgranjith89@gmail.com>
2024-05-07 05:39:16 +0530

    Created pages for Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import useFetch from "./services/useFetch";
-import { getProducts } from "./services/productService";
-import Spinner from "./Spinner";
+import Products from "./Products";
+import { Route, Routes } from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
 
 export default function App() {
-  const [size, setSize] = useState("");
-  const {
-    data: products,
-    loading,
-    error
-  } = useFetch("products?category=shoes");
-
-  const onSizeChange = (e) => {
-    setSize(e.target.value);
-  };
-
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  }
-
-  const filteredProducts = size
-    ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
-    : products;
-
-  if (error) throw error;
-  if (loading) return <Spinner />;
-
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size" value={size} onChange={onSizeChange}>
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-          </section>
-          {size && <h2>Found {filteredProducts.length} items</h2>}
-          <section id="products">{filteredProducts.map(renderProduct)}</section>
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
         </main>
       </div>
       <Footer />
