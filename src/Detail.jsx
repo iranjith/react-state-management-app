88e83ab7d2187dbfdad0630ff05084e426a163a7
iranjith <rgranjith89@gmail.com>
2024-05-07 06:19:23 +0530

    Fetch data based on URL param

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
+import PageNotFound from "./PageNotFound";
 
 export default function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams(); //destructure id from useParams
+  const { data: product, loading, error } = useFetch(`products/${id}`);
 
-  // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  if (loading) return <Spinner />;
+  if(!product) return <PageNotFound />; //if product is not found, return null (nothing will be rendered
+  if (error) throw error;
+
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
 }
